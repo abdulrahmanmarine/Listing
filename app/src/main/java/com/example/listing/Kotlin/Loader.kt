e121a28921c83e48ea231c4027eda2f1dfdff7a0
Hussain <htgopzs@gmail.com>
2021-07-13 11:16:45 +0300

    voice permission

--- app/src/main/java/com/example/listing/Kotlin/Loader.kt
+++ app/src/main/java/com/example/listing/Kotlin/Loader.kt
@@ -1,5 +1,6 @@
 package com.example.listing.Kotlin
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
